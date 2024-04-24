9509d52eba60afbc9d35bd808205b800cbe1dbf5
Salmon <sos2012happy@gmail.com>
2024-04-25 02:41:29 +0800

    update: amend Images

--- src/utils/Images.jsx
+++ src/utils/Images.jsx
@@ -1,45 +1,45 @@
 const Images = {
-  about_img: new URL("../../assets/images/about.png", import.meta.url).href,
-  play_icon: new URL("../../assets/images/play-icon.png", import.meta.url).href,
-  gallery_1: new URL("../../assets/images/gallery-1.png", import.meta.url).href,
-  gallery_2: new URL("../../assets/images/gallery-2.png", import.meta.url).href,
-  gallery_3: new URL("../../assets/images/gallery-3.png", import.meta.url).href,
-  gallery_4: new URL("../../assets/images/gallery-4.png", import.meta.url).href,
-  msg_icon: new URL("../../assets/images/msg-icon.png", import.meta.url).href,
-  mail_icon: new URL("../../assets/images/mail-icon.png", import.meta.url).href,
-  phone_icon: new URL("../../assets/images/phone-icon.png", import.meta.url)
+  about_img: new URL("../assets/images/about.png", import.meta.url).href,
+  play_icon: new URL("../assets/images/play-icon.png", import.meta.url).href,
+  gallery_1: new URL("../assets/images/gallery-1.png", import.meta.url).href,
+  gallery_2: new URL("../assets/images/gallery-2.png", import.meta.url).href,
+  gallery_3: new URL("../assets/images/gallery-3.png", import.meta.url).href,
+  gallery_4: new URL("../assets/images/gallery-4.png", import.meta.url).href,
+  msg_icon: new URL("../assets/images/msg-icon.png", import.meta.url).href,
+  mail_icon: new URL("../assets/images/mail-icon.png", import.meta.url).href,
+  phone_icon: new URL("../assets/images/phone-icon.png", import.meta.url)
     .href,
   location_icon: new URL(
-    "../../assets/images/location-icon.png",
+    "../assets/images/location-icon.png",
     import.meta.url
   ).href,
-  white_arrow: new URL("../../assets/images/white-arrow.png", import.meta.url)
+  white_arrow: new URL("../assets/images/white-arrow.png", import.meta.url)
     .href,
-  dark_arrow: new URL("../../assets/images/dark-arrow.png", import.meta.url)
+  dark_arrow: new URL("../assets/images/dark-arrow.png", import.meta.url)
     .href,
-  logo: new URL("../../assets/images/logo.png", import.meta.url).href,
-  menu_icon: new URL("../../assets/images/menu-icon.png", import.meta.url).href,
-  program_1: new URL("../../assets/images/program-1.png", import.meta.url).href,
-  program_2: new URL("../../assets/images/program-2.png", import.meta.url).href,
-  program_3: new URL("../../assets/images/program-3.png", import.meta.url).href,
+  logo: new URL("../assets/images/logo.png", import.meta.url).href,
+  menu_icon: new URL("../assets/images/menu-icon.png", import.meta.url).href,
+  program_1: new URL("../assets/images/program-1.png", import.meta.url).href,
+  program_2: new URL("../assets/images/program-2.png", import.meta.url).href,
+  program_3: new URL("../assets/images/program-3.png", import.meta.url).href,
   program_icon_1: new URL(
-    "../../assets/images/program-icon-1.png",
+    "../assets/images/program-icon-1.png",
     import.meta.url
   ).href,
   program_icon_2: new URL(
-    "../../assets/images/program-icon-2.png",
+    "../assets/images/program-icon-2.png",
     import.meta.url
   ).href,
   program_icon_3: new URL(
-    "../../assets/images/program-icon-3.png",
+    "../assets/images/program-icon-3.png",
     import.meta.url
   ).href,
-  next_icon: new URL("../../assets/images/next-icon.png", import.meta.url).href,
-  back_icon: new URL("../../assets/images/next-icon.png", import.meta.url).href,
-  user_1: new URL("../../assets/images/next-icon.png", import.meta.url).href,
-  user_2: new URL("../../assets/images/next-icon.png", import.meta.url).href,
-  user_3: new URL("../../assets/images/next-icon.png", import.meta.url).href,
-  user_4: new URL("../../assets/images/next-icon.png", import.meta.url).href,
+  next_icon: new URL("../assets/images/next-icon.png", import.meta.url).href,
+  back_icon: new URL("../assets/images/back-icon.png", import.meta.url).href,
+  user_1: new URL("../assets/images/user-1.png", import.meta.url).href,
+  user_2: new URL("../assets/images/user-2.png", import.meta.url).href,
+  user_3: new URL("../assets/images/user-3.png", import.meta.url).href,
+  user_4: new URL("../assets/images/user-4.png", import.meta.url).href,
 }
 
 export default Images;
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react()],
+  preview: {
+  port: 8080,
+  strictPort: true,
+ },
  server: {
   port: 8080,
   strictPort: true,
